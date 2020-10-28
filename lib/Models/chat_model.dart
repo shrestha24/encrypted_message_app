26ab0f2d5cfbb364aaa601ca04960ee8ee764bbb
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2020-10-28 12:06:12 +0530

    chat model completed.

---
+++ lib/Models/chat_model.dart
@@ -0,0 +1,42 @@
+import '';
+
+class ChatModel {
+  final bool isTyping;
+  final String lastMessage;
+  final String lastMessageTime;
+  final ContactModel contact;
+
+  ChatModel({
+    this.isTyping,
+    this.lastMessage,
+    this.lastMessageTime,
+    this.contact
+  }
+  );
+
+  static List<ChatModel> list = [
+
+    ChatModel(
+    isTyping: true,
+    lastMessage: "hello!",
+    lastMessageTime: "2d",
+    contact: ContactModel(name: "Shrestha"),
+  ),
+
+  ChatModel(
+      isTyping: false
+      lastMessage: "Sure!, no problm",
+      lastMessageTime: "2d",
+      contact: ContactModel(name: "Sonali"),
+      ),
+
+    ChatModel(
+      isTyping: false
+      lastMessage: "Thank you",
+      lastMessageTime: "2d",
+      contact: ContactModel(name: "Guddi"),
+    ),
+  ];
+
+
+}
