c2d19eb32827f6014cd2fe829fc1f16f8f615b40
Souarv Sharma <souravgopal25@gmail.com>
2020-10-28 13:43:05 +0530

    Decryption Completed

--- lib/page/decrypt_page.dart
+++ lib/page/decrypt_page.dart
@@ -1,13 +1,70 @@
+import 'dart:async';
+
+import 'package:encrypted_message_app/services/encrypt_service.dart';
 import 'package:flutter/material.dart';
 
-
-class DecryptPage extends StatelessWidget {
+class DecryptPage extends StatefulWidget {
   const DecryptPage({Key key}) : super(key: key);
 
   @override
+  _DecryptPageState createState() => _DecryptPageState();
+}
+
+class _DecryptPageState extends State<DecryptPage> {
+  final textController = TextEditingController();
+  final keyController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: child,
-    );
+    return Scaffold(
+        appBar: AppBar(),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextField(
+                keyboardType: TextInputType.text,
+                controller: textController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Enter Encrypted Text",
+                  labelText: "Encrypted Text",
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.text,
+                controller: keyController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Enter Key",
+                  labelText: "Key",
+                ),
+              ),
+              RaisedButton(
+                onPressed: () async {
+                  if (textController.text != null &&
+                      keyController.text != null) {
+                    var result = await EncrytService().getDecryptedText(
+                        textController.text, keyController.text);
+
+                    Timer(Duration(seconds: 2), () {
+                      showDialog(
+                          context: context,
+                          barrierDismissible: true,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text("Decrypted Text"),
+                              content: Text(
+                                result,
+                                style: TextStyle(fontSize: 25),
+                              ),
+                            );
+                          });
+                    });
+                  }
+                },
+                child: Text("Decrypt"),
+              )
+            ],
+          ),
+        ));
   }
 }
--- lib/services/encrypt_service.dart
+++ lib/services/encrypt_service.dart
@@ -35,6 +35,6 @@ class EncrytService {
     print(
         ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 
-    return '';
+    return decrypted;
   }
 }
