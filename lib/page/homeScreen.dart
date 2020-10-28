326265890f056f8297373661eb7030cd52e162da
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2020-10-28 11:57:40 +0530

    homepage completed.

--- lib/page/homeScreen.dart
+++ lib/page/homeScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -11,10 +13,10 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.mainColor,
+      backgroundColor: Colors.mainColor,
         appBar: AppBar(
         elevation: 0,
-          backgroundColor: AppColors.mainColor,
+          backgroundColor: Colors.mainColor,
           title: Text(
             "Chat",
             style: TextStyle(
@@ -24,10 +26,99 @@ class _HomeScreenState extends State<HomeScreen> {
       actions: <Widget>[
         IconButton(
             icon: Icon(
-              FlutterIcons.filter
-            ), onPressed: null)
+              FontAwesomeIcons.filter,
+              color: Colors.blue,
+            ),
+            onPressed: null),
+
       ],
               ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.all(16),
+            padding: EdgeInsets.all(6),
+            decoration: BoxDecoration(
+              color: Colors.black87,
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              )
+            ),
+         child: TextField(
+           decoration: InputDecoration(
+             prefixIcon: Icon(
+               FontAwesomeIcons.search,
+               color: Colors.white54,
+             ),
+             hintText: "Search",
+             hintStyle: TextStyle(
+               color: Colors.white54,
+             ),
+           ),
+         ),
+          ),
+          Expanded(
+            child: ListView.builder(
+          itemCount: list.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => ChatItemPage(),
+                  ),
+                );
+              },
+              leading: Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(100),
+                  ),
+                  image: DecorationImage(
+                      image: ExactAssetImage(""),
+                  ),
+                ),
+              ),
+              title: Text(
+                list[index].contact.name,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              subtitle: list[index].isTyping
+              ? Row(
+                children: <Widget>[
+                  SpinKitThreeBounce(
+                    color: Colors.blue,
+                    size:20.0,
+                  ),
+                ],
+              )
+             : Row(
+                children: <Widget>[
+                  Text(
+                    list[index].lastMessage,
+                    style: TextStyle(
+                      color: Colors.white54,
+                    ),
+                  ),
+                  SizedBox(width: 25),
+                  Text(
+                    list[index].lastMesasgeTime + "days ago",
+                    style: TextStyle(
+                      color: Colors.white54
+                    ),
+                  )
+                ],
+              )
+            )
+          }
+            ),
+          )
+        ],
+      ),
             )
           ],
         ),
